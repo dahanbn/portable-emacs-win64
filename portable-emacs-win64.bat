4a9cf160339781144957a1645bad8330d6597829
Daniel Hannaske <d.hannaske@gmail.com>
2017-12-04 22:58:37 +0100

    removed mmd from run_emacs.bat

--- portable-emacs-win64.bat
+++ portable-emacs-win64.bat
@@ -5,6 +5,6 @@ REM set HOME=%USERPROFILE%
 
 set DH_TOOLSDIR=%CD%\tools
 set GITDIR=%DH_TOOLSDIR%\PortableGit2.15.0
-set PATH=%GITDIR%\cmd;%DH_TOOLSDIR%\MultiMarkdown-Windows-6.2.2;%DH_TOOLSDIR%\pandoc-2.0.2;%DH_TOOLSDIR%\putty-0.70;%DH_TOOLSDIR%\WinSCP-5.11.2;%PATH%
+set PATH=%GITDIR%\cmd;%DH_TOOLSDIR%\pandoc-2.0.2;%DH_TOOLSDIR%\putty-0.70;%DH_TOOLSDIR%\WinSCP-5.11.2;%PATH%
 REM "emacs-25.3_1-x86_64/bin/runemacs.exe" --debug-init %*
 "emacs-25.3_1-x86_64/bin/runemacs.exe" %*
